337cb08e638a2a3d9df99cef0f2803eba6bd72a7
Rosie Hart <thenotsomiserable@gmail.com>
2021-07-09 19:59:03 +0100

    Remove broken js :)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,8 @@ $(document).on("scroll", function () {
   if
     ($(document).scrollTop() > 100) {
     $("header").addClass("shrink");
-    $(".brand").toggleClass("toggle");
   }
   else {
     $("header").removeClass("shrink");
-    $("header").addClass("brand");
   }
 });
